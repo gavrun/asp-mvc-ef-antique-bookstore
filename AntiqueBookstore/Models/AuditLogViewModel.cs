eb3286b085041f11d5dd5b405f7fe3a6b9bb05cd
gavrun <38539655+gavrun@users.noreply.github.com>
2025-04-12 03:15:06 +0100

    Added Audit controller viewmodel layout, Login filer, pagination PagedList, export to file CsvHelper

---
+++ AntiqueBookstore/Models/AuditLogViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AntiqueBookstore.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PagedList.Core;
+
+namespace AntiqueBookstore.Models
+{
+    public class AuditLogViewModel
+    {
+        public List<SalesAuditLog>? AuditLogs { get; set; }
+
+        // Paginated list of Audit logs
+        //public IPagedList<SalesAuditLog>? AuditLogs { get; set; }
+
+
+        public SelectList? LoginList { get; set; }
+
+        public string? SelectedLogin { get; set; }
+
+
+        // Properties for manual pagination 
+        // public int CurrentPage { get; set; }
+        // public int TotalPages { get; set; }
+    }
+}
